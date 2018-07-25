ebb003e593a9bf3ad44265cd1660260a2c99c1f4
Piotr Radziszewski <radnet.makis@o2.pl>
2018-07-25 11:15:49 +0200

    Create project

---
+++ src/io/github/radd/MP3Tags.java
@@ -0,0 +1,29 @@
+package io.github.radd;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Piotr
+ */
+public class MP3Tags {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(Exception e) {
+            System.out.println("Error setting native LAF: " + e);
+        }
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
+    }
+    
+}
